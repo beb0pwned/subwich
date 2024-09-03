00a33b5ac5dbbfd471b59932746000b9112c9d58
beb0pwned <mrbebap@gmail.com>
2024-09-03 14:17:52 -0500

    Added wayback and nmap functionality

--- run.sh
+++ run.sh
@@ -11,7 +11,9 @@ if [ ! -d "$url" ];then
         mkdir $url
 fi
 
-
+ if [ ! -d "$url/wayback/extensions" ];then
+        mkdir $url/wayback/extensions
+    fi
 
 echo "[+] Harvesting subdomains with subfinder..."
 sudo subfinder -d $url >> $url/subs1.txt
@@ -23,11 +25,6 @@ sudo assetfinder $url >> $url/subs2.txt
 cat $url/subs2.txt | grep $1 >> $url/final.txt
 rm $url/subs2.txt
 
-echo "[+] Checking for even more subdomains with amass..."
-sudo amass enum -d $url >> $url/subs3.txt
-cat $url/subs3.txt | grep $1 >> $url/final.txt
-rm $url/subs3.txt
-
 echo "[+] Probing for alive domains with httpx..."
 cat $url/final.txt | httpx -sc -td -ip >> $url/alive.txt
 
@@ -38,4 +35,36 @@ if [ ! -f "$url/potential_takeovers.txt" ];then
         touch $url/potential_takeovers.txt
 fi
  
-subjack -w $url/final.txt -t 100 -timeout 30 -ssl -c ~/go/src/github.com/haccer/subjack/fingerprints.json -v 3 -o $url/potential_takeovers.txt
+subjack -w $url/final.txt -t 100 -timeout 30 -ssl -c /usr/share/subjack/fingerprints.json -v 3 -o $url/potential_takeovers.txt
+
+
+echo "[+] Scraping wayback data..."
+cat $url/final.txt | waybackurls | tee -a $url/wayback/wayback.txt
+
+echo "[+] Pulling and compiling all params found in wayback data..."
+cat $url/wayback.txt | grep '?*=' | cut -d "=" -f 1 | sort -u >> $url/wayback/wayback_params.txt
+for line in $(cat $url/wayback_params.txt); do echo $line'=';done
+
+echo "[+] Pulling and compiling js/php/aspx/jsp/json files from wayback output..."
+or line in $(cat $url/wayback/wayback.txt);do
+        ext="${line##*.}"
+        if [[ "$ext" == "js" ]]; then
+            echo $line | sort -u | tee -a  $url/wayback/extensions/js.txt
+        fi
+        if [[ "$ext" == "html" ]];then
+            echo $line | sort -u | tee -a $url/wayback/extensions/jsp.txt
+        fi
+        if [[ "$ext" == "json" ]];then
+            echo $line | sort -u | tee -a $url/wayback/extensions/json.txt
+        fi
+        if [[ "$ext" == "php" ]];then
+            echo $line | sort -u | tee -a $url/wayback/extensions/php.txt
+        fi
+        if [[ "$ext" == "aspx" ]];then
+            echo $line | sort -u | tee -a $url/wayback/extensions/aspx.txt
+        fi
+    done
+
+
+echo "[+] Scanning for open ports using Nmap..."
+nmap -iL $url/alive.txt -T4 -oA $url/nmap.txt
