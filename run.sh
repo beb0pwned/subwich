39e4166e5637640edea13d6ca7d92dc0e1a09d38
beb0pwned <mrbebap@gmail.com>
2024-09-02 23:47:29 -0500

    Added subdomain takeover scan

--- run.sh
+++ run.sh
@@ -27,6 +27,7 @@ cat $url/subs2.txt | grep $1 >> $url/final.txt
 rm $url/subs2.txt
 
 echo "[+] Checking for even more subdomains with amass..."
+echo "[+] This may take some time..."
 sudo amass enum -d $url >> $url/subs3.txt
 cat $url/subs2.txt | grep $1 >> $url/final.txt
 rm $url/subs3.txt
@@ -34,3 +35,11 @@ rm $url/subs3.txt
 echo "[+] Probing for alive domains with httpx..."
 cat $url/final.txt | httpx -sc -td -ip >> $url/alive.txt
 
+
+echo "[+] Checking for possible subdomain takeover..."
+ 
+if [ ! -f "$url/potential_takeovers.txt" ];then
+        touch $url/recon/potential_takeovers.txt
+fi
+ 
+subjack -w $url/final.txt -t 100 -timeout 30 -ssl -c ~/go/src/github.com/haccer/subjack/fingerprints.json -v 3 -o $url/potential_takeovers.txt
