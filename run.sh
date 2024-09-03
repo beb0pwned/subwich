9e8b7448743409a411aebf436c52d93cb605ae06
beb0pwned <mrbebap@gmail.com>
2024-09-03 13:46:24 -0500

    Fixed a few bugs and removed the 'recon' folder

--- run.sh
+++ run.sh
@@ -11,13 +11,10 @@ if [ ! -d "$url" ];then
         mkdir $url
 fi
 
-if [ ! -d "$url/recon" ];then
-        mkdir $url/recon
-fi
 
 
-echo "[+] Harvesting subdomains with sublist3r..."
-sudo sublist3r -d $url >> $url/subs1.txt
+echo "[+] Harvesting subdomains with subfinder..."
+sudo subfinder -d $url >> $url/subs1.txt
 cat $url/subs1.txt | grep $1 >> $url/final.txt
 rm $url/subs1.txt
 
@@ -27,9 +24,8 @@ cat $url/subs2.txt | grep $1 >> $url/final.txt
 rm $url/subs2.txt
 
 echo "[+] Checking for even more subdomains with amass..."
-echo "[+] This may take some time..."
 sudo amass enum -d $url >> $url/subs3.txt
-cat $url/subs2.txt | grep $1 >> $url/final.txt
+cat $url/subs3.txt | grep $1 >> $url/final.txt
 rm $url/subs3.txt
 
 echo "[+] Probing for alive domains with httpx..."
@@ -37,9 +33,9 @@ cat $url/final.txt | httpx -sc -td -ip >> $url/alive.txt
 
 
 echo "[+] Checking for possible subdomain takeover..."
- 
+
 if [ ! -f "$url/potential_takeovers.txt" ];then
-        touch $url/recon/potential_takeovers.txt
+        touch $url/potential_takeovers.txt
 fi
  
 subjack -w $url/final.txt -t 100 -timeout 30 -ssl -c ~/go/src/github.com/haccer/subjack/fingerprints.json -v 3 -o $url/potential_takeovers.txt
