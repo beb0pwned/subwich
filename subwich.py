de02b9a7db17e4e47c1c29f1b527b4bce4c36f99
beb0pwned <mrbebap@gmail.com>
2025-02-06 01:02:52 -0600

    Fixed amass to run faster and format nicely

--- subwich.py
+++ subwich.py
@@ -1,5 +1,5 @@
 #TODO: 
-import sys
+import re
 import os
 import subprocess
 import argparse
@@ -28,7 +28,6 @@ def banner():
 """
     )
 
-
 def create_dir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
@@ -40,6 +39,16 @@ def run_command(command):
     except subprocess.CalledProcessError as e:
         print(f"{BOLD_RED}Error executing command: {e}{RESET}")
         return ""
+    
+def format_amass(input_file, output_file, output_file_2):
+    with open(input_file, "r") as infile, open(output_file, "w") as outfile, open(output_file_2, "w") as outfile_2:
+        for line in infile:
+            match = re.search(r"([\w.-]+\.\w+)\s+.*\s+(\d+\.\d+\.\d+\.\d+)", line)
+            if match:
+                domain = match.group(1)
+                ip = match.group(2)
+                outfile.write(f"{domain}")
+                outfile_2.write(f"{ip}")
 
 def main():
     try:
@@ -57,78 +66,79 @@ def main():
         
         if args.domain:
             banner()
-            url = args.domain
+            domain = args.domain
 
             # Create necessary directories
-            create_dir(url)
+            create_dir(domain)
 
-            print(f"{BOLD_TEAL}[+] Harvesting subdomains for {url} with subfinder...{RESET}")
-            subfinder_output = run_command(f"subfinder -d {url} -v")
-            with open(f"{url}/final.txt", 'w') as f:
+            print(f"{BOLD_TEAL}[+] Harvesting subdomains for {domain} with subfinder...{RESET}")
+            subfinder_output = run_command(f"subfinder -d {domain} -v")
+            with open(f"{domain}/final.txt", 'w') as f:
                 f.write(subfinder_output)
 
             print(f"{BOLD_TEAL}[+] Checking for more subdomains with assetfinder...{RESET}")
-            assetfinder_output = run_command(f"assetfinder {url}")
-            with open(f"{url}/final.txt", 'a') as f:
+            assetfinder_output = run_command(f"assetfinder {domain}")
+            with open(f"{domain}/final.txt", 'a') as f:
                 f.write(assetfinder_output)
 
             if not args.skip_amass:
                 print(f"{BOLD_TEAL}[+] Checking for even more subdomains with amass...{RESET}")
-                amass_output = run_command(f"amass enum -d {url}")
-                with open(f"{url}/final.txt", 'a') as f:
+                amass_output = run_command(f"amass enum -d {domain} -timeout 60 >> {domain}/amass.txt")
+                format_amass(input_file=f"{domain}/amass.txt",output_file=f"{domain}/final.txt",output_file_2=f"{domain}/ips.txt")
+                with open(f"{domain}/final.txt", 'a') as f:
                     f.write(amass_output)
             else:
                 print(f"{BOLD_ORANGE}[!] Skipping Amass subdomain scan.{RESET}")
 
 
             print(f"{BOLD_MAGENTA}[+] Probing for alive domains with httpx...{RESET}")
-            httpx_output = run_command(f"cat {url}/final.txt | httpx -sc -td -ip")
-            with open(f"{url}/alive.txt", 'w') as f:
+            httpx_output = run_command(f"cat {domain}/final.txt | httpx -sc -td -ip")
+            with open(f"{domain}/alive.txt", 'w') as f:
                 f.write(httpx_output)
 
             # Extract IPs and domains from httpx output
-            with open(f"{url}/ips.txt", 'w') as ip_file:
-                ips = run_command(f"cat {url}/alive.txt | grep -oE '\\b([0-9]{{1,3}}\\.){{3}}[0-9]{{1,3}}\\b'")
+            with open(f"{domain}/ips.txt", 'w') as ip_file:
+                ips = run_command(f"cat {domain}/alive.txt | grep -oE '\\b([0-9]{{1,3}}\\.){{3}}[0-9]{{1,3}}\\b'")
                 ip_file.write(ips)
 
-            with open(f"{url}/domains_alive.txt", 'w') as domain_file:
-                domains = run_command(f"cat {url}/alive.txt | sed 's|https\\?://\\([^ ]*\\).*|\\1|'")
+            with open(f"{domain}/domains_alive.txt", 'w') as domain_file:
+                domains = run_command(f"cat {domain}/alive.txt | sed 's|https\\?://\\([^ ]*\\).*|\\1|'")
                 domain_file.write(domains)
 
             print(f"{BOLD_MAGENTA}[+] Checking for possible subdomain takeover...{RESET}")
-            run_command(f"subjack -w {url}/final.txt -t 100 -timeout 30 -ssl -c '/root/go/pkg/mod/github.com/haccer/subjack@v0.0.0-20201112041112-49c51e57deab/fingerprints.json' -v 3 > {url}/potential_takeovers.txt")
+            run_command(f"subjack -w {domain}/final.txt -t 100 -timeout 30 -ssl -c '/root/go/pkg/mod/github.com/haccer/subjack@v0.0.0-20201112041112-49c51e57deab/fingerprints.json' -v 3 > {domain}/potential_takeovers.txt")
 
             
 
             # Scrape wayback data if -w flag is toggled
             if args.w:
                 # Create necessary directories
-                create_dir(f"{url}/wayback")
-                create_dir(f"{url}/wayback/extensions")
+                create_dir(f"{domain}/wayback")
+                create_dir(f"{domain}/wayback/extensions")
 
                 print(f"{BOLD_TEAL}[+] Scraping wayback data...{RESET}")
-                wayback_output = run_command(f"cat {url}/final.txt | waybackurls")
-                with open(f"{url}/wayback/wayback.txt", 'w') as f:
+                wayback_output = run_command(f"cat {domain}/final.txt | waybackdomains")
+                with open(f"{domain}/wayback/wayback.txt", 'w') as f:
                     f.write(wayback_output)
 
                 print(f"[+] Extracting parameters from wayback data...")
-                wayback_params = run_command(f"cat {url}/wayback/wayback.txt | grep '?*=' | cut -d '=' -f 1 | sort -u")
-                with open(f"{url}/wayback/wayback_params.txt", 'w') as f:
+                wayback_params = run_command(f"cat {domain}/wayback/wayback.txt | grep '?*=' | cut -d '=' -f 1 | sort -u")
+                with open(f"{domain}/wayback/wayback_params.txt", 'w') as f:
                     for line in wayback_params.splitlines():
                         f.write(line + '=\n')
 
                 print(f"[+] Extracting files with specific extensions...")
-                with open(f"{url}/wayback/wayback.txt", 'r') as wayback_file:
+                with open(f"{domain}/wayback/wayback.txt", 'r') as wayback_file:
                     for line in wayback_file:
                         line = line.strip()
                         ext = line.split('.')[-1]
                         if ext in ['js', 'html', 'json', 'php', 'aspx']:
-                            with open(f"{url}/wayback/extensions/{ext}.txt", 'a') as ext_file:
+                            with open(f"{domain}/wayback/extensions/{ext}.txt", 'a') as ext_file:
                                 ext_file.write(line + '\n')
             
-            create_dir(f"{url}/nmap")
+            create_dir(f"{domain}/nmap")
             print(f"{BOLD_TEAL}[+] Scanning for open ports using Nmap...{RESET}")
-            run_command(f"nmap -oA {url}/nmap/nmap -iL {url}/ips.txt -T4 ")
+            run_command(f"nmap -oA {domain}/nmap/nmap -iL {domain}/ips.txt -T4 ")
 
             print(f"{BOLD_ORANGE}[+] Reconnaissance complete.{RESET}")
 
